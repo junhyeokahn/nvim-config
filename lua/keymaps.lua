7f500694f9bd59e002b353840166bcf3a519eb5b
Junhyeok Ahn <junhyeokahn@macbookpro.mynetworksettings.com>
2024-11-20 19:26:17 -0500

    hello repo

---
+++ lua/keymaps.lua
@@ -0,0 +1,11 @@
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
+
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "<leader>e", ":e %:h", { desc = "[E]dit the directory of the current file" })
+vim.api.nvim_set_keymap(
+  "n",
+  "#",
+  [[:let @/='\<'.expand('<cword>').'\>'<CR>:set hlsearch<CR>]],
+  { noremap = true, silent = true }
+)
---
+++ lua/plugins/avante.lua
@@ -0,0 +1,65 @@
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  lazy = false,
+  version = false,
+  opts = {
+    provider = "ollama",
+    vendors = {
+      ---@type AvanteProvider
+      ollama = {
+        ["local"] = true,
+        endpoint = "127.0.0.1:11434/v1",
+        model = "llama3.2",
+        parse_curl_args = function(opts, code_opts)
+          return {
+            url = opts.endpoint .. "/chat/completions",
+            headers = {
+              ["Accept"] = "application/json",
+              ["Content-Type"] = "application/json",
+            },
+            body = {
+              model = opts.model,
+              messages = require("avante.providers").copilot.parse_messages(code_opts),
+              max_tokens = 2048,
+              stream = true,
+            },
+          }
+        end,
+        parse_response_data = function(data_stream, event_state, opts)
+          require("avante.providers").copilot.parse_response(data_stream, event_state, opts)
+        end,
+      },
+    },
+  },
+  build = "make",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "zbirenbaum/copilot.lua",
+    {
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+}
