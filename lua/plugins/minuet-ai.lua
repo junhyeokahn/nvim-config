66929ff6b7795b9ca30fb86569d44b2c8717b5bb
Junhyeok Ahn <junhyeokahn91@gmail.com>
2025-05-02 22:39:08 -0400

    minuet open-rotuer / tab complete

--- lua/plugins/minuet-ai.lua
+++ lua/plugins/minuet-ai.lua
@@ -9,29 +9,35 @@ return {
         enable_auto_complete = false,
       },
       virtualtext = {
-        auto_trigger_ft = { "python", "lua", "c", "cpp", "markdown" },
-        keymap = {
-          accept = "<A-a>",
-          accept_line = "<A-A>",
-          prev = "<A-[>",
-          next = "<A-]>",
-          dismiss = "<A-e>",
-        },
+        auto_trigger_ft = { "python", "cpp", "c" },
       },
-      provider = "claude",
+      provider = "openai_compatible",
+      request_timeout = 2,
       provider_options = {
-        claude = {
-          model = "claude-3-5-haiku-20241022",
-          stream = true,
-          api_key = "ANTHROPIC_API_KEY",
-          max_tokens = 56,
-          request_timeout = 2,
+        openai_compatible = {
+          api_key = "OPENROUTER_API_KEY",
+          end_point = "https://openrouter.ai/api/v1/chat/completions",
+          model = "mistralai/codestral-2501",
+          name = "Openrouter",
           optional = {
-            stop_sequences = nil,
+            max_tokens = 56,
+            top_p = 0.9,
+            provider = {
+              sort = "throughput",
+            },
           },
         },
       },
     }
+    local vt_action = require("minuet.virtualtext").action
+
+    vim.keymap.set("i", "<Tab>", function()
+      if vt_action.is_visible() then
+        vt_action.accept()
+        return ""
+      end
+      return vim.api.nvim_replace_termcodes("<Tab>", true, false, true)
+    end, { expr = true, silent = true })
   end,
   dependencies = { "nvim-lua/plenary.nvim" },
 }
